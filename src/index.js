1d6c1286fc2e3acb987f9c5fda47685d1a66083d
JWerbrouck <jeroen.werbrouck@ugent.be>
2020-03-26 16:08:26 +0100

    reverted to http

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const https = require('https')
+// const https = require('https')
 const fs = require('fs')
 const bodyParser = require('body-parser')
 
@@ -8,9 +8,9 @@ const {
     convertTo
 } = require('./convertor')
 
-const privateKey = fs.readFileSync(process.cwd() + '/config/mkcert-key.key', 'utf8')
-const certificate = fs.readFileSync(process.cwd() + '/config/mkcert-cert.crt', 'utf8')
-const credentials = {key: privateKey, cert: certificate}
+// const privateKey = fs.readFileSync(process.cwd() + '/config/mkcert-key.key', 'utf8')
+// const certificate = fs.readFileSync(process.cwd() + '/config/mkcert-cert.crt', 'utf8')
+// const credentials = {key: privateKey, cert: certificate}
 
 const app = express()
 app.use(bodyParser.json())
@@ -24,8 +24,12 @@ app.post('/IFCtoDAE', saveToFile, convertTo)
 app.post('/IFCtoGLTF', saveToFile, convertTo)
 app.post('/IFCtoXML', saveToFile, convertTo)
 
-const httpsServer = https.createServer(credentials, app)
+// const httpsServer = https.createServer(credentials, app)
 
-httpsServer.listen(4800, () => {
-    console.log('HTTPS server is up on port '+ 4800)
+// httpsServer.listen(4800, () => {
+//     console.log('HTTPS server is up on port '+ 4800)
+// })
+
+app.listen(4800, () => {
+    console.log('HTTP server is up on port '+ 4800)
 })
