6de87496bb0a78659328e976d70b30132b63ece2
jmauwerb <jeroen.werbrouck@ugent.be>
2021-10-06 17:50:40 +0200

    new gltf converter

--- src/helper.js
+++ src/helper.js
@@ -40,6 +40,7 @@ exports.executeChildProcess = (cmd_exec) => {
     return new Promise((resolve, reject) => {
         let child = cp.exec(cmd_exec, { maxBuffer: 1024 * 1024 * 50 })
         child.on('exit', function (exit_code) {
+            console.log("closed with exit code", exit_code)
             if (exit_code === 0) {
                 resolve(exit_code)
             } else {
